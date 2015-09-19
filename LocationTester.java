c4ad0ab95b978c3d91c3976a426fad3d65e44870
Iliass <tiiliass@gmail.com>
2015-09-19 09:07:52 -0700

    initial commit

---
+++ LocationTester.java
@@ -0,0 +1,7 @@
+public class locationTester
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ SimpleLocation.java
@@ -0,0 +1,9 @@
+public class SimpleLocation{
+	public double longitude;
+	public double latitude;
+
+	public SimpleLocation(double lat, double lon){
+	    this.longitude = lon; 
+	    this.latitude = lat;
+	}
+}
