e85f9bae1e6d73df7881037c78401f117fda9d20
Iliass <tiiliass@gmail.com>
2015-09-19 09:39:59 -0700

    new method

--- LocationTester.java
+++ LocationTester.java
@@ -2,6 +2,7 @@ public class locationTester
 {
 	public static void main(String[] args)
 	{
-		
+		SimpleLocation home = new SimpleLocation();
+		SimpleLocation work = new SimpleLocation();
 	}
 }
--- SimpleLocation.java
+++ SimpleLocation.java
@@ -1,9 +1,20 @@
-public class SimpleLocation{
+public class SimpleLocation
+{
 	public double longitude;
 	public double latitude;
 
+	// Default constructor
+	public SimpleLocation(double lat, double lon){
+	    this.longitude = -122.321724 ; 
+	    this.latitude = 47.697263 ;
+	}
+	// Constructor
 	public SimpleLocation(double lat, double lon){
 	    this.longitude = lon; 
 	    this.latitude = lat;
+	}
+	public double distance(SimpleLocation other)
+	{
+		return (this.latitude , this.longitude, other.latitude, other.longitude)
 	}
 }
